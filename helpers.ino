e2fe1372a8412d9e30afb1e200c39202b6d652ef
Matt Borja <matt.borja@gmail.com>
2016-01-31 02:22:12 -0700

    Update helpers.ino

--- helpers.ino
+++ helpers.ino
@@ -7,7 +7,38 @@ void blink(int total, int onDuration = 100, int offDuration = 100)
   {
     digitalWrite(LED, HIGH);
     delay(onDuration);
+    
     digitalWrite(LED, LOW);
     delay(offDuration);
+  }
+}
+
+/*
+ *  Dumps bytes hex-encoded as in 0x0000
+ */
+void dump(byte *bytes, int len)
+{
+  Serial.print("0x");
+  for (int i = 0; i < len; i++)
+  {
+    if (bytes[i] < 0x10)
+      Serial.print('0');
+
+    Serial.print(bytes[i], HEX);
+  }
+
+  Serial.print("\n");
+}
+
+/*
+ * Waits for specified number of bytes to be read from Serial.
+*/
+void waitForBytes(byte *data, int len)
+{
+  int i = 0;
+  while (i < len)
+  {
+    if (Serial.available())
+      data[i++] = Serial.read();
   }
 }
